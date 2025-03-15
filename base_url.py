8384045db3fa2c088afafa38bfccffa44325ef02
MDuymaz <81718348+MDuymaz@users.noreply.github.com>
2025-03-15 20:27:59 +0300

    Update base_url.py

--- base_url.py
+++ base_url.py
@@ -1,23 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import time
 
-# Selenium ile tarayıcıyı başlat
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
+# Chrome sürücüsünü başlat
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')  # Grafik arayüzü olmadan çalışmak için
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-# Sayfayı aç
-driver.get("https://trgoals1234.xyz/")
-time.sleep(5)  # Sayfanın tamamen yüklenmesi için bekle
+# URL'ye git
+url = "https://trgoals1234.xyz/"
+driver.get(url)
 
-# Tüm script içeriklerini al
-scripts = driver.find_elements("tag name", "script")
-with open("baseurl.txt", "w", encoding='utf-8') as f:
-    for script in scripts:
-        script_content = script.get_attribute('innerHTML')
-        if script_content:  # Boş içerik olanları yazma
-            f.write(script_content + "\n")  # Her script'ten sonra yeni satır
+# HTML içeriğini al
+html = driver.page_source
 
-# Tarayıcıyı kapat
+# HTML içeriğini 'base_url.html' dosyasına yaz
+with open('base_url.html', 'w', encoding='utf-8') as f:
+    f.write(html)
+
+# Sürücüyü kapat
 driver.quit()
